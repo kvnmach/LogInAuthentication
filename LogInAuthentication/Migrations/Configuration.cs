e4f62db97adc925e37fce5547d8d1c5c86ec9c11
kvnmach <kvnmach@gmail.com>
2016-04-20 14:56:09 -0500

    initial commit update database

---
+++ LogInAuthentication/Migrations/Configuration.cs
@@ -0,0 +1,60 @@
+using LogInAuthentication.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LogInAuthentication.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LogInAuthentication.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LogInAuthentication.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+            var userRole = new UserStore<ApplicationUser>(context);
+            var managerRole = new UserManager<ApplicationUser>(userRole);
+            var userJob = new RoleStore<IdentityRole>(context);
+            var managerJob = new RoleManager<IdentityRole>(userJob);
+
+            if (!context.Roles.Any())
+            {
+                managerJob.Create(new IdentityRole("Admin"));
+                managerJob.Create(new IdentityRole("Developer"));
+                managerJob.Create(new IdentityRole("Secretary"));
+            }
+
+            if (!context.Users.Any(x => x.UserName == "kvnmach@gmail.com"))
+            {
+                var user = new ApplicationUser
+                {
+                    FirstName = "Kevin",
+                    LastName = "Satsey",
+                    DateOfBirth = DateTime.Parse("10/10/1000"),
+                    Email = "kvnmach@gmail.com",
+                    UserName = "kvnmach@gmail.com"
+                };
+                managerRole.Create(user, "passcode");
+                managerRole.AddToRole(user.Id, "Admin");
+
+            }
+    }
+}
---
+++ LogInAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogInAuthentication.Startup))]
+namespace LogInAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
